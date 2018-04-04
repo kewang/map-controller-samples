e2e0f011e665578ca5d6e610ba9892e288712dfa
kewang <cpckewang@gmail.com>
2018-04-04 23:09:21 +0800

    Upgrade to Java 8

--- app/src/main/java/tw/kewang/mapcontroller/samples/SampleApplication.java
+++ app/src/main/java/tw/kewang/mapcontroller/samples/SampleApplication.java
@@ -1,24 +1,14 @@
 package tw.kewang.mapcontroller.samples;
 
-import tw.kewang.mapcontroller.MapController;
 import android.app.Application;
-import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import tw.kewang.mapcontroller.MapController;
 
 public class SampleApplication extends Application {
-	@Override
-	public void onCreate() {
-		super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-		try {
-			MapController.initialize(this);
-		} catch (GooglePlayServicesNotAvailableException e) {
-			e.printStackTrace();
-
-			Toast.makeText(this,
-					R.string.common_google_play_services_enable_text,
-					Toast.LENGTH_SHORT).show();
-		}
-	}
+        MapController.initialize(this);
+    }
 }
